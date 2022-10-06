0e6b3f69d777920981a92198818bd4b8a949fbea
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-10-06 21:05:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2043,12 +2043,3 @@ reply('Pesan balasan kamu diteruskan')
 }}
 }} catch (err) {
 console.log(color('[ERROR]', 'red'), err)}}
-
-//case
-let handler = async (m, { conn, command, text }) => {
-await conn.sendMessage(m.chat, { delete: m.key })
-}
-handler.customPrefix = /chat.whatsapp.com/i
-handler.command = new RegExp
-
-module.exports = handler
